fb832cb95cacf56309b16080a693ebcc23ebda49
ergate23 <ergate23@outlook.com>
2018-03-09 00:17:36 +0900

    source code ip

--- src/test/scala/SilentActor01Test.scala
+++ src/test/scala/SilentActor01Test.scala
@@ -1,5 +1,6 @@
-import akka.actor.{Actor, ActorSystem}
-import akka.testkit.TestKit
+import SilentActor.{GetState, SilentMessage}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.testkit.{TestActorRef, TestKit}
 import org.scalatest.{MustMatchers, WordSpecLike}
 
 class SilentActor01Test extends TestKit(ActorSystem("testsystem"))
@@ -8,6 +9,22 @@ class SilentActor01Test extends TestKit(ActorSystem("testsystem"))
   with StopSystemAfterAll {
 
   "A Silent Actor" must {
+
+    "change internal state when it receives a message, single" in {
+
+      val silentActor = TestActorRef[SilentActor]
+      silentActor ! SilentMessage("whisper")
+      silentActor.underlyingActor.state must (contain("whisper"))
+    }
+
+    "change internal state when it receives a message, multi" in {
+      val silentActor = system.actorOf(Props[SilentActor],"s3")
+      silentActor ! SilentMessage("whisper1")
+      silentActor ! SilentMessage("whisper2")
+      silentActor ! GetState(testActor)
+      expectMsg(Vector("whisper1","whisper2"))
+
+    }
     "change state when it receives a message, single threaded" in {
       fail("not implemented yet")
     }
@@ -17,9 +34,20 @@ class SilentActor01Test extends TestKit(ActorSystem("testsystem"))
   }
 }
 
-class SilentActor extends Actor {
-  def receive = {
-    case msg =>
-  }
+object SilentActor{
+  case class SilentMessage(data: String)
+  case class GetState(receiver: ActorRef)
+}
 
+class SilentActor extends Actor {
+
+  var internalState = Vector[String]()
+
+  def receive = {
+    case SilentMessage(data) =>
+      internalState = internalState :+ data
+    case GetState(receiver) =>
+      receiver ! internalState
+  }
+def state = internalState
 }
